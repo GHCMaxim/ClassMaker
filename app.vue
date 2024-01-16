b179856da2a7fceac32bcf244696173547d5efe4
GHCMaxim <maxim080503@gmail.com>
2024-01-16 18:47:50 +0700

    feat: :sparkles: migrate to nuxt.js; eslint; fix minor rust

---
+++ app.vue
@@ -0,0 +1,108 @@
+<script setup lang="ts">
+import { open } from "@tauri-apps/api/dialog";
+import { ref } from "vue";
+import { invoke } from "@tauri-apps/api/tauri";
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+
+interface Classes {
+	subject_id: string;
+	class_id: string;
+	included_id: string;
+	class_title: string;
+	credit: string;
+	note: string;
+	data: string;
+	location: string;
+	validity: string;
+}
+
+const classesList = ref<Classes[]>([]);
+
+const selected = ref("");
+const message = ref("");
+const textboxValue = ref("");
+const result = ref<string | null>("");
+async function select(): Promise<void> {
+	result.value = (await open({
+		multiple: false,
+		filters: [
+			{
+				name: "Spreadsheet",
+				extensions: ["xlsx", "xls", "csv"],
+			},
+		],
+	})) as string | null;
+	void parse();
+}
+
+async function parse(): Promise<void> {
+	if (result.value != null) {
+		selected.value = result.value;
+		message.value = `You selected: ${selected.value}`;
+		try {
+			const response = await invoke("parse_file", {
+				path: selected.value,
+			});
+			message.value = response as string;
+		} catch (error) {
+			console.log(error);
+		}
+	} else {
+		message.value = "You didn't select anything";
+	}
+}
+
+async function submit(): Promise<void> {
+	try {
+		const response = await invoke("sort_by_class_id", {
+			subjectId: textboxValue.value,
+		});
+		classesList.value = response as Classes[];
+		console.log(response);
+	} catch (error) {
+		console.log(error);
+	}
+}
+</script>
+
+<template>
+	<div>
+		<div class="ml-4 mt-4 flex items-center">
+			<button class="btn-s btn btn-primary" @click="select">
+				Chọn file Excel
+			</button>
+			<div v-if="message" class="ml-2">{{ message }}</div>
+		</div>
+		<div class="ml-4 mt-4 flex items-center">
+			<input
+				type="text"
+				v-model="textboxValue"
+				class="form-input"
+				placeholder="Mã học phần"
+			/>
+			<button class="btn btn-primary ml-2" @click="submit">Submit</button>
+		</div>
+		<div v-for="(item, index) in classesList" :key="index">
+			<p>{{ item.subject_id }}</p>
+			<p>{{ item.class_id }}</p>
+			<p>{{ item.included_id }}</p>
+			<p>{{ item.class_title }}</p>
+			<p>{{ item.credit }}</p>
+			<p>{{ item.note }}</p>
+			<p>{{ item.data }}</p>
+			<p>{{ item.location }}</p>
+			<p>{{ item.validity }}</p>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+.logo.vite:hover {
+	filter: drop-shadow(0 0 2em #747bff);
+}
+
+.logo.vue:hover {
+	filter: drop-shadow(0 0 2em #249b73);
+}
+</style>
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,12 +1,10 @@
-
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-use office::{Excel,  DataType};
+use office::{DataType, Excel};
 use serde::Serialize;
 use serde_json::json;
-use std::sync::Mutex;
 use std::clone::Clone;
-
+use std::sync::Mutex;
 
 trait DataTypeDisplay {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result;
@@ -28,16 +26,16 @@ impl DataTypeDisplay for DataType {
 struct Time {
     start: i32,
     end: i32,
-    day: i32
+    day: i32,
 }
 
 #[derive(Serialize)]
-struct ResultData{
+struct ResultData {
     subject_id: String,
-    class_id : String,
+    class_id: String,
     included_id: String,
-    class_title : String,
-    credit : String,
+    class_title: String,
+    credit: String,
     note: String,
     data: Vec<Time>,
     location: String,
@@ -45,7 +43,7 @@ struct ResultData{
     validity: String,
 }
 
-struct SharedState{
+struct SharedState {
     // excel is a mutex of either an excel file, or null
     excel: Mutex<Option<Excel>>,
     available_positions: Mutex<Vec<Vec<i32>>>,
@@ -56,98 +54,144 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-
-
 #[tauri::command]
 fn parse_file(state: tauri::State<'_, SharedState>, path: String) -> Result<String, String> {
     let mut shared_state = state.excel.lock().unwrap();
     *shared_state = Some(Excel::open(path).unwrap());
     if let Ok(range) = shared_state.as_mut().unwrap().worksheet_range("Sheet1") {
         let total_cells = range.get_size().0 * range.get_size().1;
-        let non_empty_cells: usize = range.rows().map(|r| {
-        r.iter().filter(|cell| cell != &&DataType::Empty).count()
-        }).sum();
-        Ok(format!("{} cells, {} non-empty cells", total_cells, non_empty_cells))
+        let non_empty_cells: usize = range
+            .rows()
+            .map(|r| r.iter().filter(|cell| cell != &&DataType::Empty).count())
+            .sum();
+        Ok(format!(
+            "{} cells, {} non-empty cells",
+            total_cells, non_empty_cells
+        ))
     } else {
         Err("Error".to_string())
     }
 }
 
-fn parse_time(date: String, time: String) -> Time{
-    let mut result = Time{
-        start: 0,
-        end: 0,
-        day: 0,
-    };
+fn parse_time(date: String, time: String) -> Time {
     let parsed_date = date.parse::<i32>().unwrap();
-    let parsed_time = time.split("-").collect::<Vec<&str>>();
+    let parsed_time = time.split('-').collect::<Vec<&str>>();
     let parsed_start_time = parsed_time[0].parse::<i32>().unwrap();
     let parsed_end_time = parsed_time[1].parse::<i32>().unwrap();
-    result = Time{
+    Time {
         start: parsed_start_time,
         end: parsed_end_time,
         day: parsed_date,
-    };
-    result
+    }
 }
 
 #[tauri::command]
-fn sort_by_class_id(state: tauri::State<'_, SharedState>, subject_id: String) -> Result<serde_json::value::Value, String> {
+fn sort_by_class_id(
+    state: tauri::State<'_, SharedState>,
+    subject_id: String,
+) -> Result<serde_json::value::Value, String> {
     let mut excel = state.excel.lock().unwrap();
-    if let Ok(range) = excel.as_mut().unwrap().worksheet_range("Sheet1"){
+    if let Ok(range) = excel.as_mut().unwrap().worksheet_range("Sheet1") {
         let mut result: Vec<ResultData> = Vec::new();
         let mut i = 0;
-        for row in range.rows(){
+        for row in range.rows() {
             if let DataType::String(value) = &row[4] {
-                if value == &subject_id{
-                    let s = if let DataType::String(value) = &row[4] {value.to_string()} else {format!("{:?}", row[4])};
-                    let class_id = if let DataType::Float(value) = &row[2] {(*value as i32).to_string()} else {format!("{:?}", row[2])};
-                    let included_id = if let DataType::Float(value) = &row[3] {(*value as i32).to_string()} else {format!("{:?}", row[3])};
-                    let class_title = if let DataType::String(value) = &row[5] {value.to_string()} else {format!("{:?}", row[5])};
-                    let credit = if let DataType::String(value) = &row[7] {value.to_string()} else {format!("{:?}", row[7])};
-                    let note = if let DataType::String(value) = &row[8] {value.to_string()} else {format!("{:?}", row[8])};
-                    let date = if let DataType::Float(value) = &row[10] {(*value as i32).to_string()} else {format!("{:?}", row[10])};
-                    let time = if let DataType::String(value) = &row[11] {value.to_string()} else {format!("{:?}", row[11])};
-                    let location = if let DataType::String(value) = &row[16] {value.to_string()} else {format!("{:?}", row[16])};
-                    let class_type = if let DataType::String(value) = &row[22] {value.to_string()} else {format!("{:?}", row[21])};
-                    let validity = if let DataType::String(value) = &row[23] {value.to_string()} else {format!("{:?}", row[23])};
+                if value == &subject_id {
+                    let s = if let DataType::String(value) = &row[4] {
+                        value.to_string()
+                    } else {
+                        format!("{:?}", row[4])
+                    };
+                    let class_id = if let DataType::Float(value) = &row[2] {
+                        (*value as i32).to_string()
+                    } else {
+                        format!("{:?}", row[2])
+                    };
+                    let included_id = if let DataType::Float(value) = &row[3] {
+                        (*value as i32).to_string()
+                    } else {
+                        format!("{:?}", row[3])
+                    };
+                    let class_title = if let DataType::String(value) = &row[5] {
+                        value.to_string()
+                    } else {
+                        format!("{:?}", row[5])
+                    };
+                    let credit = if let DataType::String(value) = &row[7] {
+                        value.to_string()
+                    } else {
+                        format!("{:?}", row[7])
+                    };
+                    let note = if let DataType::String(value) = &row[8] {
+                        value.to_string()
+                    } else {
+                        format!("{:?}", row[8])
+                    };
+                    let date = if let DataType::Float(value) = &row[10] {
+                        (*value as i32).to_string()
+                    } else {
+                        format!("{:?}", row[10])
+                    };
+                    let time = if let DataType::String(value) = &row[11] {
+                        value.to_string()
+                    } else {
+                        format!("{:?}", row[11])
+                    };
+                    let location = if let DataType::String(value) = &row[16] {
+                        value.to_string()
+                    } else {
+                        format!("{:?}", row[16])
+                    };
+                    let class_type = if let DataType::String(value) = &row[22] {
+                        value.to_string()
+                    } else {
+                        format!("{:?}", row[21])
+                    };
+                    let validity = if let DataType::String(value) = &row[23] {
+                        value.to_string()
+                    } else {
+                        format!("{:?}", row[23])
+                    };
                     let parsed_time = vec![parse_time(date, time)];
-                    let data = ResultData{
+                    let data = ResultData {
                         subject_id: s,
-                        class_id: class_id,
-                        included_id: included_id,
-                        class_title: class_title,
-                        credit: credit,
-                        note: note,
+                        class_id,
+                        included_id,
+                        class_title,
+                        credit,
+                        note,
                         data: parsed_time,
-                        location: location,
-                        class_type: class_type,
-                        validity: validity,
+                        location,
+                        class_type,
+                        validity,
                     };
                     if i == 0 {
                         result.push(data);
-                        i+=1;
-                        continue;}
-                    if result[i-1].class_id == data.class_id{
-                        result[i-1].data.push(data.data[0]);
+                        i += 1;
+                        continue;
+                    }
+                    if result[i - 1].class_id == data.class_id {
+                        result[i - 1].data.push(data.data[0]);
                         continue;
                     }
                     result.push(data);
-                    i+=1;
+                    i += 1;
                 }
+            }
         }
-        }
-        Ok(json!(result)) 
-    }
-    else {
-        Err("Error".to_string()) 
+        Ok(json!(result))
+    } else {
+        Err("Error".to_string())
     }
 }
 
-fn get_included_class(state: tauri::State<'_, SharedState>, included_id: String) -> Result<ResultData, String>{
+fn get_included_class(
+    state: tauri::State<'_, SharedState>,
+    included_id: String,
+) -> Result<ResultData, String> {
     let mut excel = state.excel.lock().unwrap();
-    if let Ok(range) = excel.as_mut().unwrap().worksheet_range("Sheet1"){
-        let mut result: ResultData = ResultData{
+    if let Ok(range) = excel.as_mut().unwrap().worksheet_range("Sheet1") {
+        let mut result: ResultData = ResultData {
             subject_id: "".to_string(),
             class_id: "".to_string(),
             included_id: "".to_string(),
@@ -160,39 +204,89 @@ fn get_included_class(state: tauri::State<'_, SharedState>, included_id: String)
             validity: "".to_string(),
         };
         let mut i = 0;
-        for row in range.rows(){
-            if let DataType::String(value) = &row[2]{
-                if value == &included_id && i == 0{
-                    let s = if let DataType::String(value) = &row[4] {value.to_string()} else {format!("{:?}", row[4])};
-                    let class_id = if let DataType::Float(value) = &row[2] {(*value as i32).to_string()} else {format!("{:?}", row[2])};
-                    let included_id = if let DataType::Float(value) = &row[3] {(*value as i32).to_string()} else {format!("{:?}", row[3])};
-                    let class_title = if let DataType::String(value) = &row[5] {value.to_string()} else {format!("{:?}", row[5])};
-                    let credit = if let DataType::String(value) = &row[7] {value.to_string()} else {format!("{:?}", row[7])};
-                    let note = if let DataType::String(value) = &row[8] {value.to_string()} else {format!("{:?}", row[8])};
-                    let date = if let DataType::Float(value) = &row[10] {(*value as i32).to_string()} else {format!("{:?}", row[10])};
-                    let time = if let DataType::String(value) = &row[11] {value.to_string()} else {format!("{:?}", row[11])};
-                    let location = if let DataType::String(value) = &row[16] {value.to_string()} else {format!("{:?}", row[16])};
-                    let class_type = if let DataType::String(value) = &row[22] {value.to_string()} else {format!("{:?}", row[21])};
-                    let validity = if let DataType::String(value) = &row[23] {value.to_string()} else {format!("{:?}", row[23])};
+        for row in range.rows() {
+            if let DataType::String(value) = &row[2] {
+                if value == &included_id && i == 0 {
+                    let s = if let DataType::String(value) = &row[4] {
+                        value.to_string()
+                    } else {
+                        format!("{:?}", row[4])
+                    };
+                    let class_id = if let DataType::Float(value) = &row[2] {
+                        (*value as i32).to_string()
+                    } else {
+                        format!("{:?}", row[2])
+                    };
+                    let included_id = if let DataType::Float(value) = &row[3] {
+                        (*value as i32).to_string()
+                    } else {
+                        format!("{:?}", row[3])
+                    };
+                    let class_title = if let DataType::String(value) = &row[5] {
+                        value.to_string()
+                    } else {
+                        format!("{:?}", row[5])
+                    };
+                    let credit = if let DataType::String(value) = &row[7] {
+                        value.to_string()
+                    } else {
+                        format!("{:?}", row[7])
+                    };
+                    let note = if let DataType::String(value) = &row[8] {
+                        value.to_string()
+                    } else {
+                        format!("{:?}", row[8])
+                    };
+                    let date = if let DataType::Float(value) = &row[10] {
+                        (*value as i32).to_string()
+                    } else {
+                        format!("{:?}", row[10])
+                    };
+                    let time = if let DataType::String(value) = &row[11] {
+                        value.to_string()
+                    } else {
+                        format!("{:?}", row[11])
+                    };
+                    let location = if let DataType::String(value) = &row[16] {
+                        value.to_string()
+                    } else {
+                        format!("{:?}", row[16])
+                    };
+                    let class_type = if let DataType::String(value) = &row[22] {
+                        value.to_string()
+                    } else {
+                        format!("{:?}", row[21])
+                    };
+                    let validity = if let DataType::String(value) = &row[23] {
+                        value.to_string()
+                    } else {
+                        format!("{:?}", row[23])
+                    };
                     let parsed_time = vec![parse_time(date, time)];
-                    let data = ResultData{
+                    let data = ResultData {
                         subject_id: s,
-                        class_id: class_id,
-                        included_id: included_id,
-                        class_title: class_title,
-                        credit: credit,
-                        note: note,
+                        class_id,
+                        included_id,
+                        class_title,
+                        credit,
+                        note,
                         data: parsed_time,
-                        location: location,
-                        class_type: class_type,
-                        validity: validity,
+                        location,
+                        class_type,
+                        validity,
                     };
                     result = data;
-                    }
-
-                else if value == &included_id && i != 0{
-                    let date = if let DataType::Float(value) = &row[10] {(*value as i32).to_string()} else {format!("{:?}", row[10])};
-                    let time = if let DataType::String(value) = &row[11] {value.to_string()} else {format!("{:?}", row[11])};
+                } else if value == &included_id && i != 0 {
+                    let date = if let DataType::Float(value) = &row[10] {
+                        (*value as i32).to_string()
+                    } else {
+                        format!("{:?}", row[10])
+                    };
+                    let time = if let DataType::String(value) = &row[11] {
+                        value.to_string()
+                    } else {
+                        format!("{:?}", row[11])
+                    };
                     let parsed_time = vec![parse_time(date, time)];
                     result.data.push(parsed_time[0]);
                     continue;
@@ -200,28 +294,36 @@ fn get_included_class(state: tauri::State<'_, SharedState>, included_id: String)
             }
         }
         Ok(result)
-    } else{
+    } else {
         Err("Error".to_string())
     }
 }
-fn check_validity(state: tauri::State<'_, SharedState>, time: Vec<Time>) -> bool{
+
+fn check_validity(state: tauri::State<'_, SharedState>, time: Vec<Time>) -> bool {
     let mut valid = true;
     let mut available_positions = state.available_positions.lock().unwrap();
-    for i in 0..time.len(){
-        if available_positions[time[i].day as usize -2].contains(&time[i].start) || available_positions[time[i].day as usize -2].contains(&time[i].end){
+    for i in 0..time.len() {
+        if available_positions[time[i].day as usize - 2].contains(&time[i].start)
+            || available_positions[time[i].day as usize - 2].contains(&time[i].end)
+        {
             valid = false;
             break;
         }
         available_positions[time[i].day as usize - 2].push(time[i].start);
         available_positions[time[i].day as usize - 2].push(time[i].end);
         available_positions[time[i].day as usize - 2].sort();
-        for j in 0..available_positions[time[i].day as usize - 2].len(){
-            if available_positions[time[i].day as usize - 2][j] == time[i].start && available_positions[time[i].day as usize - 2][j+1] == time[i].end{
+        for j in 0..available_positions[time[i].day as usize - 2].len() {
+            if available_positions[time[i].day as usize - 2][j] == time[i].start
+                && available_positions[time[i].day as usize - 2][j + 1] == time[i].end
+            {
                 break;
             }
             valid = false;
             available_positions[time[i].day as usize - 2].remove(j);
-            let index = available_positions[time[i].day as usize - 2].iter().position(|x| *x == time[i].end).unwrap();
+            let index = available_positions[time[i].day as usize - 2]
+                .iter()
+                .position(|x| *x == time[i].end)
+                .unwrap();
             available_positions[time[i].day as usize - 2].remove(index);
         }
     }
@@ -235,7 +337,11 @@ fn main() {
             excel: Mutex::new(None),
             available_positions: Mutex::new(initial_positions),
         })
-        .invoke_handler(tauri::generate_handler![greet, parse_file, sort_by_class_id])
+        .invoke_handler(tauri::generate_handler![
+            greet,
+            parse_file,
+            sort_by_class_id
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
