a428477b6a400b432f1ed0a78b8208e41804c5ac
GHCMaxim <maxim080503@gmail.com>
2024-01-17 14:16:05 +0700

    feat(tauri): Added function to add classes

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,7 @@
 use office::{DataType, Excel};
 use serde::Serialize;
 use serde_json::json;
+use serde::Deserialize;
 use std::clone::Clone;
 use std::sync::Mutex;
 
@@ -22,14 +23,14 @@ impl DataTypeDisplay for DataType {
         }
     }
 }
-#[derive(Serialize, Copy, Clone)]
+#[derive(Serialize, Copy, Clone, Deserialize)]
 struct Time {
     start: i32,
     end: i32,
     day: i32,
 }
 
-#[derive(Serialize)]
+#[derive(Serialize, Clone, Deserialize)]
 struct ResultData {
     subject_id: String,
     class_id: String,
@@ -47,6 +48,7 @@ struct SharedState {
     // excel is a mutex of either an excel file, or null
     excel: Mutex<Option<Excel>>,
     available_positions: Mutex<Vec<Vec<i32>>>,
+    chosen_classes: Mutex<Vec<ResultData>>,
 }
 
 #[tauri::command]
@@ -142,7 +144,7 @@ fn sort_by_class_id(
                     } else {
                         format!("{:?}", row[16])
                     };
-                    let class_type = if let DataType::String(value) = &row[22] {
+                    let class_type = if let DataType::String(value) = &row[21] {
                         value.to_string()
                     } else {
                         format!("{:?}", row[21])
@@ -186,7 +188,7 @@ fn sort_by_class_id(
 }
 
 fn get_included_class(
-    state: tauri::State<'_, SharedState>,
+    state: &tauri::State<'_, SharedState>,
     included_id: String,
 ) -> Result<ResultData, String> {
     let mut excel = state.excel.lock().unwrap();
@@ -252,7 +254,7 @@ fn get_included_class(
                     } else {
                         format!("{:?}", row[16])
                     };
-                    let class_type = if let DataType::String(value) = &row[22] {
+                    let class_type = if let DataType::String(value) = &row[21] {
                         value.to_string()
                     } else {
                         format!("{:?}", row[21])
@@ -276,6 +278,7 @@ fn get_included_class(
                         validity,
                     };
                     result = data;
+                    i+=1;
                 } else if value == &included_id && i != 0 {
                     let date = if let DataType::Float(value) = &row[10] {
                         (*value as i32).to_string()
@@ -299,7 +302,7 @@ fn get_included_class(
     }
 }
 
-fn check_validity(state: tauri::State<'_, SharedState>, time: Vec<Time>) -> bool {
+fn check_validity(state: &tauri::State<'_, SharedState>, time: Vec<Time>) -> bool {
     let mut valid = true;
     let mut available_positions = state.available_positions.lock().unwrap();
     for i in 0..time.len() {
@@ -330,17 +333,48 @@ fn check_validity(state: tauri::State<'_, SharedState>, time: Vec<Time>) -> bool
     valid
 }
 
+#[tauri::command]
+fn add_chosen_class(state: tauri::State<'_, SharedState>, data: ResultData) -> Result<String, String> {
+    let mut failed_classes = Vec::new();
+    let mut chosen_classes = state.chosen_classes.lock().unwrap();
+    let mut classes = Vec::new();
+    let mut chosen_ids = Vec::new();
+    classes.push(data.clone());
+    if data.class_id != data.included_id || data.included_id != "NULL"{
+        let included = get_included_class(&state, data.included_id).unwrap();
+        classes.push(included);
+    }
+    for i in 0..classes.len(){
+        if !check_validity(&state, classes[i].clone().data){
+            failed_classes.push(classes[i].clone().class_id); 
+        }          
+    }
+    if failed_classes.len() == 0{
+        for i in 0..classes.len(){
+            chosen_classes.push(classes[i].clone());
+        }
+        for i in 0..chosen_classes.len(){
+            chosen_ids.push(chosen_classes[i].clone().class_id);
+        }
+        Ok(format!("Các lớp đã chọn: {:?}", chosen_ids))
+    }else{
+        Err(format!("Failed classes: {:?}", failed_classes))
+    }
+}
 fn main() {
     let initial_positions: Vec<Vec<i32>> = vec![Vec::new(); 6];
+    let chosen_classes: Vec<ResultData> = Vec::new();
     tauri::Builder::default()
         .manage(SharedState {
             excel: Mutex::new(None),
             available_positions: Mutex::new(initial_positions),
+            chosen_classes: Mutex::new(chosen_classes),
         })
         .invoke_handler(tauri::generate_handler![
             greet,
             parse_file,
-            sort_by_class_id
+            sort_by_class_id,
+            add_chosen_class
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
